2bc0c827b6ebe989256002c3d558b6868e93cef3
e4rache <e4rache@protonmail.com>
2018-07-11 11:20:57 +0200

    t

---
+++ api/routes/orders.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'handling GET /orders'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'handling POST /orders'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId
+    res.status(200).json({
+        id: id,
+        message: 'handling GET /orders/:orderId' 
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId
+    res.status(200).json({
+        id: id,
+        message: 'handling DELETE /orders/:orderId' 
+    })
+})
+
+module.exports = router
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,38 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(201).json({              // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
         message: 'Handling POST request to /products'
+    })
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'handling GET /products/special'
+        })
+    } else {
+        res.status(200).json({
+            id: id,
+            message: 'handling GET /products/:productId'
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    res.status(200).json({
+        id : id,
+        message: 'handling PATCH /products/:productID'
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    res.status(200).json({
+        id:id,
+        message: 'handling DELETE /products/:productId'
     })
 })
 
