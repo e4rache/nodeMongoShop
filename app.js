56ea41d087cc7d7b849617bd1977faeaac6c8bc4
e4rache <e4rache@protonmail.com>
2018-07-11 11:25:42 +0200

    added products routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
@@ -7,4 +7,4 @@ const orderRoutes = require('./api/routes/orders')
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
-module.exports = app;
+module.exports = app
